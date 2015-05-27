311dd6ca3586e1001c33a211a1dd69ecf21124bc
Chase Higgins <chasehx@gmail.com>
2015-05-27 11:58:12 -0600

    build out contact overview

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -10,76 +10,32 @@
     'foundation.dynamicRouting',
     'foundation.dynamicRouting.animations'
   ])
-    .config(config)
+    .config(config)	
     .run(run)
 	
 		// factories 
 		.factory('ContactService', function($http) {
 			return {
 				getContacts: function(params, callback) {
-					// test fixtures
-					var contacts = [
-						{
-							first_name: "Chase",
-							last_name: "Higgins",
-							email: "chase@wreet.co",
-							subject: "Website Development",
-							create_date: "05/23/2015",
-							message: "Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor. Suspendisse dictum feugiat nisl ut dapibus. Mauris iaculis porttitor posuere. Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Etiam at risus et justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum laoreet. Nunc eu.",
-							assigned: false,
-							id: "fldkj3324"
-						},
-						{
-							first_name: "Jon",
-							last_name: "Cron",
-							email: "jon@wreet.co",
-							subject: "Mobile App Development",
-							create_date: "05/22/2015",
-							message: "Yo dude I need you to make me a mobile app.",
-							assigned: false,
-							id: "r0923jwed"
-						},
-						{
-							first_name: "Customer",
-							last_name: "McTesting",
-							email: "test@lolfactory.com",
-							subject: "Consulting",
-							create_date: "05/18/2015",
-							message: "I just want to look into having someone review our company's code base. ",
-							assigned: "Chase",
-							id: "flkfds93s"
-						}
-					];
-					// end test fixtures
-					if (params.assigned !== false) {
-						var assigned_contacts = [];
-						for (var i = 0; i < contacts.length; i++) {
-							if (contacts[i].assigned == params.assigned) {
-								assigned_contacts.push(contacts[i]);	
-							}
-						}
-						contacts = assigned_contacts;
-					}
-					else if (params.assigned === false) {
-						// only return open contacts	
-						var unassigned = [];
-						for (var j = 0; j < contacts.length; j++) {
-							if (contacts[j].assigned == params.assigned) {
-								unassigned.push(contacts[j]);	
-							}
-						}
-						contacts = unassigned;
-					}
-					// end params.assigned handling
+					var contacts = [];
+					// handle parameters
 					
-					
-					callback(contacts);
+					$http.post('http://localhost:3000/api/v1/contacts', {search_params: params, api_token: "78bb831366f4defd38ba3a1d414986e2"}).
+					success(function(data, status, headers, config) {
+						contacts = angular.fromJson(data);
+						console.log(contacts);
+						callback(contacts);
+					}).
+					error(function(data, status, headers, config) {
+						console.log('Request failed: ' + status);
+					});
 				}, // end getContacts method
 				
 				setSelectedContact: function(contact_id, contacts) {
 					for (var i = 0; i < contacts.length; i++) {
-						if (contacts[i].id == contact_id)
+						if (contacts[i]._id.$oid == contact_id) {
 							return contacts[i];
+						}
 					}
 					// we don't wanna be here
 					return false;
